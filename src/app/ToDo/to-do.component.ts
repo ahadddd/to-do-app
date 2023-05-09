f690ac4dacb6a7eb38830d5dae817d3dd635f51b
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-09 17:13:57 +0500

    task complete

--- src/app/ToDo/to-do.component.ts
+++ src/app/ToDo/to-do.component.ts
@@ -7,24 +7,32 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./to-do.component.css']
 })
 export class ToDoComponent implements OnInit {
-  items: any = ['Hello', 'World', 'Test', 'To-do', 'item', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6'];
+  items: any = [];
   editMode = false;
   itemData!: FormGroup;
   holder: number = 0;
   invalid = false;
+  checked = false;
 
   ngOnInit(): void {
 
     this.itemData = new FormGroup({
-      item: new FormControl('', [Validators.required, Validators.maxLength(200)])
+      item: new FormControl('Enter To-Do Item', [Validators.required, Validators.maxLength(5)]),
+      checked: new FormControl(false)
     });
+    console.log(this.item);
+    
+  }
+
+  get item() {
+    return this.itemData.controls['item'];
   }
 
   addItem() {
-
     const itemControl = this.itemData.controls['item'];
+    const strikeControl = this.itemData.controls['checked'];
     if (itemControl.invalid) {
-      alert('Field is required.')
+      console.log(itemControl.errors)
     }
     // let exists = false;
     // this.items.forEach((item: any) => {
@@ -41,10 +49,11 @@ export class ToDoComponent implements OnInit {
     //   alert('To do already exists.')
     // }
     if (itemControl.valid) {
-      this.items.push(itemControl.getRawValue())
+      this.items.push(this.itemData.getRawValue())
       this.itemData.reset()
     }
   }
+
 
   save() {
     this.items[this.holder] = this.itemData.controls['item'].getRawValue();
