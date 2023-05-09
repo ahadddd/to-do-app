0a95ba8ec6bdf9f8048ae33dcdf20fa913603225
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-09 12:31:07 +0500

    edit function added

--- src/app/ToDo/to-do.component.ts
+++ src/app/ToDo/to-do.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do',
@@ -7,19 +7,59 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./to-do.component.css']
 })
 export class ToDoComponent implements OnInit {
-  items: any = [];
-
+  items: any = ['Hello', 'World', 'Test', 'To-do', 'item'];
+  editMode = false;
   itemData!: FormGroup;
+  holder: number = 0;
 
   ngOnInit(): void {
 
     this.itemData = new FormGroup({
-      item: new FormControl()
+      item: new FormControl('', [Validators.required])
     });
   }
 
   addItem() {
-    this.items.push( this.itemData.controls['item'].getRawValue());
+
+    const itemControl = this.itemData.controls['item'];
+    if (itemControl.invalid) {
+      alert('Field is required.')
+    }
+    // let exists = false;
+    // this.items.forEach((item: any) => {
+    //   if (this.itemData.controls['item'].getRawValue() == item) {
+    //     exists = true;
+    //     return;
+    //   }
+    // })
+    // if (exists == false) {
+    //   this.items.push(this.itemData.controls['item'].getRawValue());
+    //   alert('To do added.')
+    // }
+    // else {
+    //   alert('To do already exists.')
+    // }
+    if (itemControl.valid) {
+      this.items.push(itemControl.getRawValue())
+      this.itemData.reset()
+    }
+  }
+
+  save() {
+    this.items[this.holder] = this.itemData.controls['item'].getRawValue();
+    this.holder = 0;
+    this.editMode = false;
+    this.itemData.reset();
+  }
+
+  editToDo(index: any) {
+    this.itemData.setValue({ item: this.items[index] })
+    this.editMode = true;
+    this.holder = index;
+  }
+
+  deleteToDo(index: any) {
+    this.items.splice(index, 1);
   }
 
 }
